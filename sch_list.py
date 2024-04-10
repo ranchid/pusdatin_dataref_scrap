0fec45147239747d77d7aa59f6951b3d5f0e346a
Nazarudin Rasid <ranchiddas@live.com>
2024-04-10 21:28:29 +0700

    feat: add intermediate method for get all dataset

--- sch_list.py
+++ sch_list.py
@@ -1,4 +1,5 @@
 import re
+import asyncio
 from datetime import datetime
 
 import httpx
@@ -6,7 +7,7 @@ from bs4 import BeautifulSoup
 
 __base_url = "https://referensi.data.kemdikbud.go.id/pendidikan"
 
-direktorat = ['paud', 'dikdas', 'dikmen', 'dikmas']
+direktorat = ['paud', 'dikdas', 'dikmen']
 
 async def get_sch_list(dikbud_id3:str, direktorat:str, cust_header:dict=None, verify:bool=False):
     async with httpx.AsyncClient(headers=cust_header, verify=verify, timeout=None) as client:
@@ -37,7 +38,7 @@ async def get_sch_list(dikbud_id3:str, direktorat:str, cust_header:dict=None, ve
 
         return data
     
-async def get_sch_detail(sch_meta:dict, cust_header:dict=None, verify:bool=None):
+async def get_sch_detail(sch_meta:dict, cust_header:dict=None, verify:bool=False):
     async with httpx.AsyncClient(headers=cust_header, verify=verify, timeout=None) as client:
         raw_resp = await client.get(f"{sch_meta['link']}")
         _content = raw_resp.content
@@ -53,6 +54,11 @@ async def get_sch_detail(sch_meta:dict, cust_header:dict=None, verify:bool=None)
         i_province = _identity[7].findAll('td')[3].contents[0]
         i_status = _identity[8].findAll('td')[3].contents[0]
         i_inst_type = _identity[9].findAll('td')[3].contents[0]
+        try:
+            i_serv = re.split(r", | dan ", _identity[10].findAll('td')[3].contents[0])
+            i_services = [prog for prog in i_serv if prog != '']
+        except IndexError:
+            i_services = None
         _legal = raw_data[1].contents[1].findAll('tr')
         l_ministry_spv = _legal[0].findAll('td')[3].contents[0]
 
@@ -60,21 +66,34 @@ async def get_sch_detail(sch_meta:dict, cust_header:dict=None, verify:bool=None)
             l_inst_spv = _legal[1].findAll('td')[3].contents[0]
         except IndexError:
             l_inst_spv = None
-
-        l_inst_id = _legal[2].findAll('td')[3].contents[0]
+        
         try:
-            l_foundation_lt = _legal[3].findAll('td')[3].contents[0]
+            l_foundation = _legal[2].findAll('td')[3].contents[1]
+            l_fnd_id = l_foundation.contents[0].strip()
+            l_fnd_detail = l_foundation.get('href')
         except IndexError:
-            l_foundation_lt = None
+            l_fnd_id = None
+            l_fnd_detail = None
+        
         try:
-            l_foundation_date = datetime.strptime(_legal[4].findAll('td')[3].contents[0], "%d-%m-%Y").date()
+            l_establish_lt = _legal[3].findAll('td')[3].contents[0]
+        except IndexError:
+            l_establish_lt = None
+        
+        try:
+            l_establish_date = datetime.strptime(_legal[4].findAll('td')[3].contents[0], "%d-%m-%Y").date()
         except ValueError:
-            l_foundation_date = None
-        l_opr_id = _legal[5].findAll('td')[3].contents[0]
+            l_establish_date = None
+        try:
+            l_opr_id = _legal[5].findAll('td')[3].contents[0]
+        except IndexError:
+            l_opr_id = None
+        
         try:
             l_opr_date = datetime.strptime(_legal[6].findAll('td')[3].contents[0], "%d-%m-%Y").date()
         except:
             l_opr_date = None
+        
         l_opr_lt = _legal[7].findAll('td')[3].contents[1]
 
         if l_opr_lt.contents[0] != "Silakan Upload SK (link file tidak valid)":
@@ -86,6 +105,10 @@ async def get_sch_detail(sch_meta:dict, cust_header:dict=None, verify:bool=None)
         except ValueError:
             l_opr_lt_up_date = None
         _contact = raw_data[3].contents[1].findAll('tr')
+        try:
+            c_email = _contact[2].findAll('td')[3].contents[0]
+        except IndexError:
+            c_email = None
         try:
             c_website = _contact[3].findAll('td')[3].contents[0].get('href')
         except IndexError:
@@ -110,17 +133,20 @@ async def get_sch_detail(sch_meta:dict, cust_header:dict=None, verify:bool=None)
             'kab_kota': i_city_reg,
             'provinsi': i_province,
             'btk_pend': i_inst_type,
+            'pr_layanan': i_services,
             'status': i_status,
             'kementerian': l_ministry_spv,
             'naungan': l_inst_spv,
-            'npyp': l_inst_id,
-            'no_sk_pendirian': l_foundation_lt,
-            'tgl_sk_pendirian': l_foundation_date,
+            'npyp': l_fnd_id,
+            'detail_yayasan': l_fnd_detail,
+            'no_sk_pendirian': l_establish_lt,
+            'tgl_sk_pendirian': l_establish_date,
             'no_sk_ijop': l_opr_id,
             'tgl_sk_ijop': l_opr_date,
             'tautan_sk_ijop': l_opr_lt_link,
             'tgl_unggah_ijop': l_opr_lt_up_date,
             'website': c_website,
+            'email': c_email,
             'tautan_skita': i_skita_link,
             'induk_kec': sch_meta['induk_kec'],
             'bujur': longitude,
@@ -128,3 +154,19 @@ async def get_sch_detail(sch_meta:dict, cust_header:dict=None, verify:bool=None)
             }
 
         return data
+    
+def get_dataset(subdistricts:list, w_units:list, get_detail:bool, batch_limit:int=400, cust_header:dict=None, ssl_verif:bool=False) -> dict:
+    sch_list_crawl = [get_sch_list(subdist, w_unit, cust_header, ssl_verif) for subdist in subdistricts for w_unit in w_units]
+    crawl_exec = asyncio.run(asyncio.gather(*sch_list_crawl))
+    sch_list = [sch for sch_sublist in crawl_exec for sch in sch_sublist]
+    match get_detail:
+        case False:
+            return sch_list
+        case True:
+            throttle = [sch_list[sch:sch+batch_limit] for sch in range(0, len(sch_list), batch_limit)]
+            data = []
+            for subset in throttle:
+                crawl_detail = [get_sch_detail(sch, cust_header=cust_header) for sch in subset]
+                detail_info = asyncio.run(asyncio.gather(*crawl_detail))
+                data += detail_info
+            return data
